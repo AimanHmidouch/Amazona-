75ff7b2fe2259991ac31095f3733958930034fdc
aiman <aiman.hmidouch96@gmail.com>
2020-12-12 18:43:54 +0100

    before Part 16

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { addToCart } from '../actions/cartActions';
+import { useDispatch } from 'react-redux'
 
 export default function CartScreen(props) {
     const productId = props.match.params.id;
-    const qty = props.location.search ? Number(props.location.search.split("=")[1]) : 1
+    console.log(productId);
+    const qty = props.location.search ? Number(props.location.search.split("=")[1]) : 1;
+    console.log(qty);
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
     return (
         <div>
             <h1>{qty} </h1>
