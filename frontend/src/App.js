100825f81f1d071122167a37a9212bfe63295e30
aiman <aiman.hmidouch96@gmail.com>
2020-12-13 00:15:55 +0100

    before part 22

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import SigningScreen from './screens/SigningScreen';
 
 function App() {
   const cart = useSelector(state => state.cart)
@@ -27,6 +28,7 @@ function App() {
         <main>
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
+          <Route path="/signin" component={SigningScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
 
         </main>
