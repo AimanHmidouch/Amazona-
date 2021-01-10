363560e84d23cb3174dc3f72bb5c747cc860ebc7
aiman <aiman.hmidouch96@gmail.com>
2021-01-10 23:31:19 +0100

    before 34

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,6 +11,7 @@ import OrderScreen from './screens/OrderScreen';
 import PaymentMethodScreen from './screens/PaymentMethodScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import ProductScreen from './screens/ProductScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ShippingAddressScreen from './screens/ShippingAddressScreen';
 import SigningScreen from './screens/SigningScreen';
@@ -41,6 +42,9 @@ function App() {
                 <Link to="#">{userInfo.name} <i className="fa fa_caret-down"></i></Link>
                 <ul className="dropdown-content">
                   <li>
+                    <Link to="/profile">User Profile</Link>
+                  </li>
+                  <li>
                     <Link to="/orderhistory">Order History</Link>
                   </li>
                   <li>
@@ -65,6 +69,7 @@ function App() {
           <Route path="/placeorder" component={PlaceOrderScreen}></Route>
           <Route path="/order/:id" component={OrderScreen}></Route>
           <Route path="/orderhistory" component={OrderHistoryScreen}></Route>
+          <Route path="/profile" component={ProfileScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
 
         </main>
